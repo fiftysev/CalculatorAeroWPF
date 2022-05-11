01acf96236c532e573fe72cf04b8662f027ff0c0
fiftysev <nesgorai@bk.ru>
2022-05-11 19:40:55 +1000

    First try to handling dynamic row/col span

--- CalculatorApp/MainWindow.xaml.cs
+++ CalculatorApp/MainWindow.xaml.cs
@@ -9,27 +9,32 @@ namespace CalculatorApp
     /// </summary>
     public partial class MainWindow
     {
-        private string[][] _buttonsLayout =
+        private readonly string[][] _buttonsLayout =
         {
             new string[] {"MC", "MR", "MS", "M+", "M-"},
             new string[] {"🠔", "CE", "C", "±", "√"},
             new string[] {"7", "8", "9", "/", "%"},
             new string[] {"4", "5", "6", "*", "1/x"},
             new string[] {"1", "2", "3", "-", "="},
-            new string[] {"0", "0", ",", "+", "="}
+            new string[] {"0", "0", ".", "+", "="}
         };
+
         public MainWindow()
         {
             InitializeComponent();
             InitUi();
         }
 
-        public void InitUi()
+        private void InitUi()
         {
             for (var i = 0; i < _buttonsLayout.Length; i++)
             {
                 for (var j = 0; j < _buttonsLayout[i].Length; j++)
                 {
+                    if (i > 0 && j > 0) 
+                    {
+                        if  (_buttonsLayout[i][j - 1] == _buttonsLayout[i][j] || _buttonsLayout[i - 1][j] == _buttonsLayout[i][j]) continue;
+                    }
                     var b = new Button
                     {
                         Content = _buttonsLayout[i][j],
@@ -39,16 +44,16 @@ namespace CalculatorApp
                     Grid.SetRow(b, i);
                     Grid.SetColumn(b, j);
                     
-                    if (j + 1 != _buttonsLayout[i].Length && _buttonsLayout[i][j] == _buttonsLayout[i][j + 1])
+                    if (j + 1 != _buttonsLayout[i].Length && _buttonsLayout[i][j + 1] == _buttonsLayout[i][j])
                     {
-                        var prevRowSpan = Grid.GetRowSpan(b);
-                        Grid.SetRowSpan(b, ++prevRowSpan);
+                        var prevColSpan = Grid.GetRowSpan(b);
+                        Grid.SetColumnSpan(b, ++prevColSpan);
                     }
 
-                    if (i + 1 != _buttonsLayout.Length && _buttonsLayout[i][j] == _buttonsLayout[i + 1][j])
+                    if (i + 1 != _buttonsLayout.Length && _buttonsLayout[i + 1][j] == _buttonsLayout[i][j])
                     {
-                        var prevColSpan = Grid.GetColumnSpan(b);
-                        Grid.SetColumnSpan(b, ++prevColSpan);
+                        var prevRowSpan = Grid.GetColumnSpan(b);
+                        Grid.SetRowSpan(b, ++prevRowSpan);
                     }
 
                     RootLayout.Children.Add(b);
