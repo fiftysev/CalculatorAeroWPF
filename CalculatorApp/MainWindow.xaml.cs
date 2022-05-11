b4f81d4b10a552ff12bc97d196aeab704dda41d3
fiftysev <nesgorai@bk.ru>
2022-05-11 18:35:00 +1000

    Create basic app layout/ Add UI init methods

--- CalculatorApp/MainWindow.xaml.cs
+++ CalculatorApp/MainWindow.xaml.cs
@@ -1,13 +1,59 @@
-﻿namespace CalculatorApp
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CalculatorApp
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow
     {
+        private string[][] _buttonsLayout =
+        {
+            new string[] {"MC", "MR", "MS", "M+", "M-"},
+            new string[] {"🠔", "CE", "C", "±", "√"},
+            new string[] {"7", "8", "9", "/", "%"},
+            new string[] {"4", "5", "6", "*", "1/x"},
+            new string[] {"1", "2", "3", "-", "="},
+            new string[] {"0", "0", ",", "+", "="}
+        };
         public MainWindow()
         {
             InitializeComponent();
+            InitUi();
+        }
+
+        public void InitUi()
+        {
+            for (var i = 0; i < _buttonsLayout.Length; i++)
+            {
+                for (var j = 0; j < _buttonsLayout[i].Length; j++)
+                {
+                    var b = new Button
+                    {
+                        Content = _buttonsLayout[i][j],
+                        Margin = new Thickness(2.5)
+                    };
+                    
+                    Grid.SetRow(b, i);
+                    Grid.SetColumn(b, j);
+                    
+                    if (j + 1 != _buttonsLayout[i].Length && _buttonsLayout[i][j] == _buttonsLayout[i][j + 1])
+                    {
+                        var prevRowSpan = Grid.GetRowSpan(b);
+                        Grid.SetRowSpan(b, ++prevRowSpan);
+                    }
+
+                    if (i + 1 != _buttonsLayout.Length && _buttonsLayout[i][j] == _buttonsLayout[i + 1][j])
+                    {
+                        var prevColSpan = Grid.GetColumnSpan(b);
+                        Grid.SetColumnSpan(b, ++prevColSpan);
+                    }
+
+                    RootLayout.Children.Add(b);
+                }
+            }
         }
     }
 }
